03f70f2260508ab16b0bbae8ffebd7f0e6b240fa
Matt Zeunert <matt@mostlystatic.com>
2019-01-24 15:34:47 +0000

    Tweak

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           React app
+          <div>more stuff</div>
         </header>
       </div>
     );
