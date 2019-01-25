86f6af9aff33c56618232de6405c52822283c1a3
Matt Zeunert <matt@mostlystatic.com>
2019-01-25 12:55:28 +0000

    tttt

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           React app
-          <div>more stuffaaaaaarrrr</div>
+          <div>more stuffaaaaaarrrrtttt</div>
         </header>
       </div>
     );
