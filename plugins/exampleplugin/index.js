5bc092f486dc2890b0d514dc59cf967ad8d27251
Matt Zeunert <matt@mostlystatic.com>
2020-09-01 11:04:07 +0100

    asdf

--- plugins/exampleplugin/index.js
+++ plugins/exampleplugin/index.js
@@ -1,11 +1,28 @@
-function netlifyPlugin(conf) {
-    return {
-        // Hook into lifecycle
-        onEnd: function () {
+const {DebugBear} = require('debugbear');
+
+const {
+    DEBUGBEAR_API_KEY,
+    DEBUGBEAR_PAGE_IDS,
+    COMMIT_REF
+} = process.env
+
+
+module.exports = {
+    async onSuccess() {
+        try {
             console.log("ENV", process.env)
             console.log("site build finished, site deployed! 🚀", arguments)
+
+            const dbb = new DebugBear(DEBUGBEAR_API_KEY)
+            const pageIds = DEBUGBEAR_PAGE_IDS.split(",")
+
+            // await Promise.all(pageIds.map(pageId => {
+            //     dbb.pages.analyze(pageId)
+            // }))
+
+            console.log("Running DebugBear tests")
+        } catch (error) {
+            return failBuild('DebugBear tests failed', { error })
         }
     }
 }
-
-module.exports = netlifyPlugin
