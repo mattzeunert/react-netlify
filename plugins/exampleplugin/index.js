83d95c0421ae7522ec79b372ec4dd33c6c224d77
Matt Zeunert <matt@mostlystatic.com>
2020-09-01 10:40:46 +0100

    asdf

--- plugins/exampleplugin/index.js
+++ plugins/exampleplugin/index.js
@@ -2,6 +2,7 @@ function netlifyPlugin(conf) {
     return {
         // Hook into lifecycle
         onEnd: function () {
+            console.log("ENV", process.env)
             console.log("site build finished, site deployed! 🚀", arguments)
         }
     }
